68f909ed2537487917cff15cc3adae0d035de1f8
ssbabu95 <babu.sumeet1@gmail.com>
2015-10-24 16:07:34 -0400

    URL changes

--- cs4501/main.py
+++ cs4501/main.py
@@ -6,13 +6,13 @@ import json
 
 
 def render_home(request):
-	req = urllib.request.Request('http://10.0.2.2:8002/home')
+	req = urllib.request.Request('http://exp-api:8000/home')
 	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
 	resp = json.loads(resp_json)
 	return render(request, 'home.html', resp["resp"])
 
 def item_det(request, listing_id):
-	req = urllib.request.Request('http://10.0.2.2:8002/listing/' + listing_id)
+	req = urllib.request.Request('http://exp-api:8000/listing/' + listing_id)
 	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
 	resp = json.loads(resp_json)
 	return render(request, 'det.html', resp["resp"])
